01c3bac8d687e2fc3b247405ab364efbc18d7aa9
walther <walcarher@gmail.com>
2020-11-10 13:34:11 -0500

    include image size and channel depth

--- conv_gen.py
+++ conv_gen.py
@@ -1,20 +1,19 @@
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
-import time
 
-input_tensor = 224 # input image/tensor size i.e. 224x224 for ImageNet
-#start_num_convs = 100 # starting number of filters or depth of the output tensor
-#max_num_convs = 3000 # max number of filters or depth of the output tensor
-num_conv_list = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+
+HW_in = [224, 112, 56, 28, 14] # input image/tensor size i.e. 224x224 for ImageNet
+C_in = [3, 16, 32, 64, 128, 256]
+#start_n = 100 # starting number of filters or depth of the output tensor
+#max_n = 3000 # max number of filters or depth of the output tensor
+N = [16, 32, 64, 128, 256]
 #step_size_convs = 100 # step size from start to maximum number of iterations
 #n_iter = 5000  # Number of iterations on a single convolution run
 	       # the average of results is reported in output file
 
 
-# Random input tensor or image with 1 batch, 1 channel and size 
-# input_tensorxinput_tensor
-input = torch.randn(1,1,input_tensor,input_tensor)
+
 input_names = ['input']
 output_names = ['output']
 
@@ -23,119 +22,124 @@ output_names = ['output']
 #		yield start
 #		start += step
 
-#for num_convs in iter_range(start_num_convs, max_num_convs, step_size_convs):
-for num_convs in num_conv_list:
+for hw in HW_in:
+	for c in C_in:
+		# Random input tensor or image with 1 batch, c channel and size 
+		# hxw
+		input = torch.randn(1,c,hw,hw)
+		#for n in iter_range(start_n, max_n, step_size_convs):
+		for n in N:
 
-	print("Number of convolutions: %d" % num_convs)
+			print("Number of convolutions: %d" % n)
 
-	class Conv1x1_Net(nn.Module):
+			class Conv1x1_Net(nn.Module):
 
-		def __init__(self):
-			super(Conv1x1_Net,self).__init__()
-			#1 batch size, n conv output, kernel size 1x1, stride 1-1
-			self.conv1 = nn.Conv2d(1, num_convs, 1)
+				def __init__(self):
+					super(Conv1x1_Net,self).__init__()
+					#1 batch size, n conv output, kernel size 1x1, stride 1-1
+					self.conv1 = nn.Conv2d(c, n, 1)
 
-		def forward(self, x):
-			# Maxpooling 2x2 and ReLu activation function
-			#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
-			x = self.conv1(x)
-			return x
+				def forward(self, x):
+					# Maxpooling 2x2 and ReLu activation function
+					#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+					x = self.conv1(x)
+					return x
 
-	class Conv3x3_Net(nn.Module):
+			class Conv3x3_Net(nn.Module):
 
-		def __init__(self):
-			super(Conv3x3_Net,self).__init__()
-			#1 batch size, n conv output, kernel size 3x3, stride 1-1
-			self.conv1 = nn.Conv2d(1, num_convs, 3)
+				def __init__(self):
+					super(Conv3x3_Net,self).__init__()
+					#1 batch size, n conv output, kernel size 3x3, stride 1-1
+					self.conv1 = nn.Conv2d(c, n, 3)
 
-		def forward(self, x):
-			# Maxpooling 2x2 and ReLu activation function
-			#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
-			x = self.conv1(x)			
-			return x
+				def forward(self, x):
+					# Maxpooling 2x2 and ReLu activation function
+					#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+					x = self.conv1(x)			
+					return x
 
-	class Conv5x5_Net(nn.Module):
+			class Conv5x5_Net(nn.Module):
 
-		def __init__(self):
-			super(Conv5x5_Net,self).__init__()
-			#1 batch size, n conv output, kernel size 5x5, stride 1-1
-			self.conv1 = nn.Conv2d(1, num_convs, 5)
+				def __init__(self):
+					super(Conv5x5_Net,self).__init__()
+					#1 batch size, n conv output, kernel size 5x5, stride 1-1
+					self.conv1 = nn.Conv2d(c, n, 5)
 
-		def forward(self, x):
-			# Maxpooling 2x2 and ReLu activation function
-			#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
-			x = self.conv1(x)			
-			return x
+				def forward(self, x):
+					# Maxpooling 2x2 and ReLu activation function
+					#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+					x = self.conv1(x)			
+					return x
 
-	class Conv7x7_Net(nn.Module):
+			class Conv7x7_Net(nn.Module):
 
-		def __init__(self):
-			super(Conv7x7_Net,self).__init__()
-			#1 batch size, n conv output, kernel size 7x7, stride 1-1
-			self.conv1 = nn.Conv2d(1, num_convs, 7)
+				def __init__(self):
+					super(Conv7x7_Net,self).__init__()
+					#1 batch size, n conv output, kernel size 7x7, stride 1-1
+					self.conv1 = nn.Conv2d(c, n, 7)
 
-		def forward(self, x):
-			# Maxpooling 2x2 and ReLu activation function
-			#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
-			x = self.conv1(x)			
-			return x
+				def forward(self, x):
+					# Maxpooling 2x2 and ReLu activation function
+					#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+					x = self.conv1(x)			
+					return x
 
-	class Conv11x11_Net(nn.Module):
+			class Conv11x11_Net(nn.Module):
 
-		def __init__(self):
-			super(Conv11x11_Net,self).__init__()
-			#1 batch size, n conv output, kernel size 11x11, stride 1-1
-			self.conv1 = nn.Conv2d(1, num_convs, 11)
+				def __init__(self):
+					super(Conv11x11_Net,self).__init__()
+					#1 batch size, n conv output, kernel size 11x11, stride 1-1
+					self.conv1 = nn.Conv2d(c, n, 11)
 
-		def forward(self, x):
-			# Maxpooling 2x2 and ReLu activation function
-			#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
-			x = self.conv1(x)			
-			return x
+				def forward(self, x):
+					# Maxpooling 2x2 and ReLu activation function
+					#x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+					x = self.conv1(x)			
+					return x
 
-	# Convolution layer model
-	conv1x1_net = Conv1x1_Net()
-	print(conv1x1_net)
+			# Convolution layer model
+			conv1x1_net = Conv1x1_Net()
+			print(conv1x1_net)
 
-	print("Now generating ...")
+			print("Now generating ...")
 
-	onnx_file = 'vhdl_generated/conv1x1/conv1x1_%d' % num_convs + '.onnx'
-	torch.onnx.export(conv1x1_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
+			onnx_file = 'vhdl_generated/conv1x1/hw_%d/c_%d/conv1x1_%d_%d_%d' % (hw, c, hw, c, n) + '.onnx'
+			torch.onnx.export(conv1x1_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
 
-	# Convolution layer model
-	conv3x3_net = Conv3x3_Net()
-	print(conv3x3_net)
+			# Convolution layer model
+			conv3x3_net = Conv3x3_Net()
+			print(conv3x3_net)
 	
-	print("Now generating ...")
+			print("Now generating ...")
 
-	onnx_file = 'vhdl_generated/conv3x3/conv3x3_%d' % num_convs + '.onnx'
-	torch.onnx.export(conv3x3_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
+			onnx_file = 'vhdl_generated/conv3x3/hw_%d/c_%d/conv3x3_%d_%d_%d' % (hw, c, hw, c, n) + '.onnx'
+			torch.onnx.export(conv3x3_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
 
-	# Convolution layer model
-	conv5x5_net = Conv5x5_Net()
-	print(conv5x5_net)
+			# Convolution layer model
+			conv5x5_net = Conv5x5_Net()
+			print(conv5x5_net)
 
-	print("Now generating ...")
+			print("Now generating ...")
 
-	onnx_file = 'vhdl_generated/conv5x5/conv5x5_%d' % num_convs + '.onnx'
-	torch.onnx.export(conv5x5_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
+			onnx_file = 'vhdl_generated/conv5x5/hw_%d/c_%d/conv5x5_%d_%d_%d' % (hw, c, hw, c, n) + '.onnx'
+			torch.onnx.export(conv5x5_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
 
-	# Convolution layer model
-	conv7x7_net = Conv7x7_Net()
-	print(conv7x7_net)
+			# Convolution layer model
+			conv7x7_net = Conv7x7_Net()
+			print(conv7x7_net)
 
-	print("Now generating ...")
+			print("Now generating ...")
 
-	onnx_file = 'vhdl_generated/conv7x7/conv7x7_%d' % num_convs + '.onnx'
-	torch.onnx.export(conv7x7_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
+			onnx_file = 'vhdl_generated/conv7x7/hw_%d/c_%d/conv7x7_%d_%d_%d' % (hw, c, hw, c, n) + '.onnx'
+			torch.onnx.export(conv7x7_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
 
-	# Convolution layer model
-	conv11x11_net = Conv11x11_Net()
-	print(conv11x11_net)
+			# Convolution layer model
+			conv11x11_net = Conv11x11_Net()
+			print(conv11x11_net)
 
-	print("Now generating ...")
+			print("Now generating ...")
 
-	onnx_file = 'vhdl_generated/conv11x11/conv11x11_%d' % num_convs + '.onnx'
-	torch.onnx.export(conv11x11_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
+			onnx_file = 'vhdl_generated/conv11x11/hw_%d/c_%d/conv11x11_%d_%d_%d' % (hw, c, hw, c, n) + '.onnx'
+			torch.onnx.export(conv11x11_net, input, onnx_file, verbose=True, input_names=input_names, output_names=output_names)
 
 print("Generation Done")
--- delirium_conv_compiler.sh
+++ delirium_conv_compiler.sh
@@ -1,20 +1,35 @@
 mkdir "vhdl_generated"
-mkdir "vhdl_generated/conv1x1"
-mkdir "vhdl_generated/conv3x3"
-mkdir "vhdl_generated/conv5x5"
-mkdir "vhdl_generated/conv7x7"
-mkdir "vhdl_generated/conv11x11"
-python conv_gen.py
-for i in 2 4 8 16 32 64 128 256 512 1024;
+
+CONV=("conv1x1" "conv3x3" "conv5x5" "conv7x7" "conv11x11")
+HW=(224 112 56 28 14)
+C=(3 16 32 64 128 256)
+N=(16 32 64 128 256)
+
+for conv in "${CONV[@]}";
 do
-mkdir "vhdl_generated/conv1x1/conv1x1_"$i"_vhdl_generated";
-python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv1x1/conv1x1_"$i".onnx" --out "vhdl_generated/conv1x1/conv1x1_"$i"_vhdl_generated" --nbits 8;
-mkdir "vhdl_generated/conv3x3/conv3x3_"$i"_vhdl_generated";
-python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv3x3/conv3x3_"$i".onnx" --out "vhdl_generated/conv3x3/conv3x3_"$i"_vhdl_generated" --nbits 8;
-mkdir "vhdl_generated/conv5x5/conv5x5_"$i"_vhdl_generated";
-python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv5x5/conv5x5_"$i".onnx" --out "vhdl_generated/conv5x5/conv5x5_"$i"_vhdl_generated" --nbits 8;
-mkdir "vhdl_generated/conv7x7/conv7x7_"$i"_vhdl_generated";
-python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv7x7/conv7x7_"$i".onnx" --out "vhdl_generated/conv7x7/conv7x7_"$i"_vhdl_generated" --nbits 8;
-mkdir "vhdl_generated/conv11x11/conv11x11_"$i"_vhdl_generated";
-python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv11x11/conv11x11_"$i".onnx" --out "vhdl_generated/conv11x11/conv11x11_"$i"_vhdl_generated" --nbits 8;
+	mkdir "vhdl_generated/"$conv
+	for hw in "${HW[@]}";
+	do
+		mkdir "vhdl_generated/"$conv"/hw_"$hw
+		for c in "${C[@]}";
+		do 
+			mkdir "vhdl_generated/"$conv"/hw_"$hw"/c_"$c
+		done
+	done
+done
+
+python3 conv_gen.py
+
+for n in "${N[@]}"
+do
+	mkdir "vhdl_generated/conv1x1/conv1x1_"$n"_vhdl_generated";
+	python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv1x1/conv1x1_"$n".onnx" --out "vhdl_generated/conv1x1/conv1x1_"$n"_vhdl_generated" --nbits 8;
+	mkdir "vhdl_generated/conv3x3/conv3x3_"$n"_vhdl_generated";
+	python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv3x3/conv3x3_"$n".onnx" --out "vhdl_generated/conv3x3/conv3x3_"$n"_vhdl_generated" --nbits 8;
+	mkdir "vhdl_generated/conv5x5/conv5x5_"$n"_vhdl_generated";
+	python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv5x5/conv5x5_"$n".onnx" --out "vhdl_generated/conv5x5/conv5x5_"$n"_vhdl_generated" --nbits 8;
+	mkdir "vhdl_generated/conv7x7/conv7x7_"$n"_vhdl_generated";
+	python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv7x7/conv7x7_"$n".onnx" --out "vhdl_generated/conv7x7/conv7x7_"$n"_vhdl_generated" --nbits 8;
+	mkdir "vhdl_generated/conv11x11/conv11x11_"$n"_vhdl_generated";
+	python3 "../delirium_dist/delirium.py" --onnx "vhdl_generated/conv11x11/conv11x11_"$n".onnx" --out "vhdl_generated/conv11x11/conv11x11_"$n"_vhdl_generated" --nbits 8;
 done
